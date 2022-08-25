37a363d8c45459379a547733f6a619e53beeff95
jeffersonahimar <jeffersonahimar@outlook.com>
2022-08-25 02:14:14 -0500

    Etapa 5 finalizada

---
+++ Entidades/Alumno.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CoreEscuela.Entidades
+{
+    public class Alumno: ObjetoEscuelaBase
+    {
+        public List<Evaluación> Evaluaciones { get; set; } =new List<Evaluación>();
+    }
+}
